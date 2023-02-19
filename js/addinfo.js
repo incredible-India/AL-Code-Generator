940efd407d365230cc2651ce70413cc3a4414536
himanshu sharma <iamonlyhimanshusharma@gmail.com>
2023-02-19 15:43:10 +0530

    added for field error

--- js/addinfo.js
+++ js/addinfo.js
@@ -1,7 +1,9 @@
 let submitbutton =  document.getElementById("secondstep");
 var objectName = document.getElementById('oname');
 var objectid = document.getElementById("id");
-
+var objectType = document.getElementById("object");
+var moreinfo = document.getElementsByClassName('moreinfo')[0];
+var count =0;
 //when user will click for the next step
 submitbutton.onclick = (e)=>{
 
@@ -23,8 +25,93 @@ submitbutton.onclick = (e)=>{
 
         //if everything is correct then ask for the further properties...
 
-        
+        // if table is selected 
+
+        if(objectType.value == "table")
+        {
+            tableInfo(objectid,objectName);
+
+        }else
+        {
+            alert("Sorry Except Table We are not Providing Service");
+        }
 
     }
 
+}
+
+
+
+// function for the table 
+
+function tableInfo(id,name)
+{
+
+
+    //first we will ask for the field name
+
+     moreinfo.innerHTML ='';
+    addMoreFields(count);
+    moreinfo.innerHTML += `  <div class="container mt-3">
+    <input type="button" id="addmore" value="Add Filed" class="btn btn-warning mt-2 mx-2">
+    <input type="button" value="Next Step" class="btn btn-info mt-2 mx-2">
+</div>`;
+
+//for add more field
+try {
+    
+    document.getElementById('addmore').addEventListener('click',(e)=>{
+
+        count =  count+1;
+        addMoreFields(count);
+
+
+    })
+
+} catch (error) {
+    
+}
+
+
+
+}
+
+
+function addMoreFields(id)
+{
+    id +=1;
+
+    moreinfo.innerHTML += `
+    <div class="container"> 
+    
+    
+    <div class="row">
+
+    <div class="col-sm-3">
+        <label for="">Field No. <span class="text-danger">*</span></label>
+        <input type="number" name="" id="no${id}" class="form-control">
+    </div>
+    <div class="col-sm-3">
+        <label for="">Field Name <span class="text-danger">*</span></label>
+        <input type="text" name="" id="name${id}" class="form-control">
+    </div>
+
+    <div class="col-sm-3">
+        <label for="">Data Type <span class="text-danger">*</span></label>
+        <input type="number" name="" id="type${id}" class="form-control">
+    </div>
+
+    <div class="col-sm-3">
+        <label for="">Data Length <span class="text-danger">*</span></label>
+        <input type="number" name="" id="length${id}" class="form-control">
+    </div>
+   
+   </div>
+    
+    
+    </div>
+
+     
+    
+`
 }
